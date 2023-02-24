2f878cda7cdbc3886acff57f8fa14a4cc3319538
Kamirru <->
2023-02-24 15:44:02 +0100

    def display_balance(self):

--- ex1.py
+++ ex1.py
@@ -4,6 +4,11 @@ class BankAccount:
         self.account_number = account_number
         self.balance = 0.0
 
+    def display_balance(self):
+        print(f"{self.account_number} {self.account_name}")
+        print(f"current balance: {self.balance}")
+
 # Lily Smith
 account = BankAccount("LilySmith",9988887777)
+account.display_balance()
 
