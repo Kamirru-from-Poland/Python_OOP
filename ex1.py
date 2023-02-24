b5489f03dd29c89d6cd608dde65047e365fdba07
Kamirru <->
2023-02-24 15:06:13 +0100

    added BankAccount

---
+++ ex1.py
@@ -0,0 +1,9 @@
+class BankAccount:
+    def __init__(self, account_name:str, account_number:int):
+        self.account_name = account_name
+        self.account_number = account_number
+        self.balance = 0.0
+
+# Lily Smith
+account = BankAccount("LilySmith",9988887777)
+
